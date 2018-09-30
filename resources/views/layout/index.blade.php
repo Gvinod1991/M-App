e973061f9098a8ed399d653b036aac0aa07eeb11
Godti Vinod <venkat.godti3@gmail.com>
2018-09-30 20:21:08 +0530

    index view updated

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -7,12 +7,7 @@
  <body class="theme-green">
  <!-- Preloder-->
 <!-- Page Loader -->
-<div class="page-loader-wrapper">
-    <div class="loader">
-        <div class="m-t-30"><img src="../images/icon-light.svg" width="48" height="48" alt="HexaBit"></div>
-        <p>Please wait...</p>        
-    </div>
-</div>
+@include('layout.preloader')
 <!-- Overlay For Sidebars -->
 <div class="overlay"></div>
 
